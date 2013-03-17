dff787fab058d6a812d116847085fd1c73d75f2f
dima <dgrachikov@gmail.com>
2013-03-17 20:22:26 +0400

    init 3

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DG\JiraAuthBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('DGJiraAuthBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
