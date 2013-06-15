ff09f023143e297ffb1210b55cff2742ba3a133d
DmitryGrachikov <dgrachikov@gmail.com>
2013-06-16 00:14:33 +0400

    Добавлены методы просмотра открытх и защищенных страниц Добавлены шаблоны страниц

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -10,4 +10,12 @@ class DefaultController extends Controller
     {
         return $this->render('DGJiraAuthBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function showPublicPageAction($alias){
+        return $this->render('DGJiraAuthBundle:Public:' . $alias . '.html.twig');
+    }
+
+    public function showPrivatePageAction($alias){
+        return $this->render('DGJiraAuthBundle:Private:' . $alias . '.html.twig');
+    }
 }
