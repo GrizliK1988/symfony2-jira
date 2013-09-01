4034daac215b762f4d1b079e48515f49f5b4e633
DmitryGrachikov <dgrachikov@gmail.com>
2013-09-01 20:09:47 +0400

    Зарегистрирована фабрика

--- DGJiraAuthBundle.php
+++ DGJiraAuthBundle.php
@@ -2,8 +2,17 @@
 
 namespace DG\JiraAuthBundle;
 
+use DG\JiraAuthBundle\DependencyInjection\Security\Factory\JiraFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DGJiraAuthBundle extends Bundle
 {
+    public function build(ContainerBuilder $container){
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+
+        $extension->addSecurityListenerFactory(new JiraFactory());
+    }
 }
